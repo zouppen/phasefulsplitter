9dc4c0053a59a613f9e1b6ad46bc209180925e3a
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-03-31 13:10:04 +0300

    More beautiful code, guards and lifts :-)

--- haskell/ApacheParser.hs
+++ haskell/ApacheParser.hs
@@ -12,6 +12,7 @@ import Control.Monad (liftM)
 
 -- import Data.Binary
 
+-- |Converts a bytestring containing an Apache date to UTCTime
 fromApacheTime :: B.ByteString -> Maybe UTCTime
 fromApacheTime s = parseTime defaultTimeLocale "%d/%b/%Y:%T %z" $ B.unpack s
 
@@ -19,6 +20,7 @@ fromApacheTime s = parseTime defaultTimeLocale "%d/%b/%Y:%T %z" $ B.unpack s
 -- because [^]] is broken in TDFA (or it is broken in grep).
 apacheLogRegex = compileNicely "^([0-9\\.]+) ([^ ]+) +([^ ]+) +\\[([A-Za-z0-9 +-:/]*)\\] \"([^\"]*)\" ([0-9]{3}) ([0-9]+|-) \"(.*)\" \"(.*)\"$"
 
+-- |Compiles String into a TDFA Regex with good options.
 compileNicely :: String -> Regex
 compileNicely regexText = fromEither $ compile CompOption {caseSensitive = False, multiline = True, rightAssoc = True, newSyntax = True, lastStarGreedy = False} ExecOption {captureGroups = True} (B.pack regexText)
 
@@ -28,10 +30,13 @@ fromEither :: Either String t -> t
 fromEither = either error id
 
 -- |Reads an entry from a given ByteString. Returns result and the
--- rest of the ByteString as a pair.
+-- rest of the ByteString as a pair. Used by unfoldr and therefore it
+-- returns Just even if there will be parse error in that line. Parse
+-- errors are marked with Left and processing will continue even if
+-- parsing fails.
 getEntry :: B.ByteString -> Maybe (Either (String,B.ByteString) Entry, B.ByteString)
-getEntry bs = if bs == B.empty then Nothing
-              else Just $ getEntry' bs
+getEntry bs | bs == B.empty = Nothing
+            | otherwise     = Just $ getEntry' bs
 
 getEntry' bs = case match of
                  Left msg -> (Left (msg,curLine),nextLine)
@@ -41,29 +46,39 @@ getEntry' bs = case match of
           curLine = B.takeWhile (/='\n') bs
           nextLine = B.tail $ B.dropWhile (/='\n') bs
 
-
-toEntry [a,_,_,b,c,d,e,f,g] = maybeEntry (Just a) (fromApacheTime b) (liftM reqMethod req) (liftM reqURL req) (liftM reqProtocol req)  (readInteger d) (readInteger e) (Just f) (Just g)
-                              where req = readRequest c
+-- |Creates an entry from regex match groups
+toEntry :: [B.ByteString] -> Maybe Entry
+toEntry [rawIP,_,_,rawDate,rawReq,rawResponse,rawBytes,rawReferer,rawBrowser] =
+    maybeEntry
+    (Just rawIP)
+    (fromApacheTime rawDate)
+    (liftM  (!! 0) req) -- method
+    (liftM  (!! 1) req) -- URL
+    (liftM  (!! 2) req) -- protocol
+    (readInteger rawResponse)
+    (readInteger rawBytes)
+    (Just rawReferer)
+    (Just rawBrowser)
+    where req = splitBS requestRegex rawReq
 
 requestRegex = compileNicely "^([A-Z]+) (.+) ([^ ]+)"
 
-readRequest :: B.ByteString -> Maybe [B.ByteString]
-readRequest bs = case match of 
+-- |Splits a given bytestring to a list of groups with a Regex.
+splitBS :: Regex -> B.ByteString -> Maybe [B.ByteString]
+splitBS regex bs = case match of 
                    Left s -> Nothing
                    Right Nothing -> Nothing
                    Right (Just (_,_,_,ms)) -> Just ms
-  where match = regexec requestRegex bs
-        
+  where match = regexec regex bs
 
-reqMethod (x:_) = x
-reqURL (_:x:_) = x
-reqProtocol (_:_:x:_) = x
-
-readInteger bs = if bs == B.pack "-" then Just 0
-                 else case (B.readInteger bs) of
-                        Nothing -> Nothing
-                        Just (i,rest) -> if rest == B.empty then Just i
-                                         else Nothing
+-- |Reads integer from ByteString, failing if the entire ByteString is
+-- not parsed as a string.
+readInteger :: B.ByteString -> Maybe Integer
+readInteger bs | bs == B.pack "-" = Just 0
+readInteger bs = case (B.readInteger bs) of
+                   Nothing -> Nothing
+                   Just (i,rest) -> if rest == B.empty then Just i
+                                    else Nothing
 
 -- |Stupid way to convert Nothing to Left.
 eitherifyMaybe _ (Just x) = Right x
@@ -71,6 +86,7 @@ eitherifyMaybe err Nothing = Left err
 
 toTimeStamp t = formatTime defaultTimeLocale "%s" t
 
+-- |Unwraps Maybes and takes Nothing in the front of Entry.
 maybeEntry (Just ip) (Just date) (Just method) (Just url) (Just protocol) (Just response) (Just bytes) (Just referer) (Just browser) = Just $ Entry ip date method url protocol response bytes referer browser
 maybeEntry _ _ _ _ _ _ _ _ _ = Nothing
 
