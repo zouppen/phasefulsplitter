fc5749b91da5ff8237f48349b756cda692ceaf48
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-04-22 16:33:33 +0300

    More feature-rich n-gram implementation. Vectors are supported now.

--- src/Ngram.hs
+++ src/Ngram.hs
@@ -4,15 +4,45 @@
 
 module Ngram where
 
-data GramElement a = Begin | El a | End  deriving (Show,Eq)
+import qualified Data.Map as M
 
--- |Transforms a list into gram list suitable for grammifying. Wraps
--- |list inside GramElement so Begin and End is taken care.
-toGramList xs = Begin : toGramList' xs
-toGramList' [] = [End]
-toGramList' (x:xs) = (El x):(toGramList' xs)
+data Location = Begin | In | End  deriving (Show,Ord,Eq)
+data Ngram a = Ngram Location [a]  deriving (Show,Ord,Eq)
 
--- |Produces n-grams in /raw/ form. That is, begin and end is not
--- |there.
-grammify n xs = take (length xs-n+1) $ grammify' n xs
-grammify' n xs = (take n xs):(grammify' n $ tail xs)
+-- |Transforms a list into list of slining window n-grams. Wraps
+-- |individual n-grams inside Ngram which takes care of Begin and End
+-- |tokens.
+nGrams :: Int -> [a] -> [Ngram a]
+nGrams n xs = (Ngram Begin [head xs]):(Ngram End [last xs]):(map (Ngram In) $ rawNgram n xs)
+
+-- |Produces n-grams in /raw/ form. That is an n-gram without Begin
+-- |and End tokens.
+rawNgram :: Int -> [a] -> [[a]]
+rawNgram n xs = take (length xs-n+1) $ rawNgram' n xs
+rawNgram' n xs = (take n xs):(rawNgram' n $ tail xs)
+
+-- |Produces frequency map from n-grams (or any other instance of Eq)
+frequencyMap :: (Ord a) => [a] -> M.Map a Integer
+frequencyMap xs = M.fromListWith (+) $ map (\x -> (x,1)) xs
+
+-- |Forms frequency table of n-grams from a single list.
+frequencyNgram :: (Ord a) => Int -> [a] -> M.Map (Ngram a) Integer
+frequencyNgram n xs = frequencyMap $ nGrams n xs
+
+-- |Forms frequency table of n-grams from many lists. Takes care of
+-- |Begins and Ends properly (every list has its own Begin and End
+-- |tokens.
+giganticFrequencyNgram :: (Ord a) => Int -> [[a]] -> M.Map (Ngram a) Integer
+giganticFrequencyNgram n xss = frequencyMap $ concat $ map (nGrams n) xss
+
+-- |Map a map to contain only zeros. Used in generation on n-gram vectors.
+toZeroMap :: M.Map k a -> M.Map k Integer
+toZeroMap = M.map (const 0)
+
+-- Returns a vectorized (=list) form of n-gram. The second parameter
+-- is zero map containing all possible n-grams as keys. Zero map can
+-- be generated from 'giganticFrequencyNgram' result with
+-- 'toZeroMap'. Vectorized form can be used as output to dimension
+-- reduction algorithms.
+nGramVector :: (Ord k) => M.Map k a -> M.Map k a -> [a]
+nGramVector zeroMap x = M.elems $ M.union x zeroMap
