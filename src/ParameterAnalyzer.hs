6e6bc20c8902f97bf9a6399fe678d1d6758132d2
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-04-30 15:05:32 +0300

    Hopefully this now outputs valid summary about the data.

--- src/ParameterAnalyzer.hs
+++ src/ParameterAnalyzer.hs
@@ -2,19 +2,35 @@
 
 module Main where
 
-import Data.Map
+import qualified Data.Map as M
 import System.Environment
 import Network.URL
 import PhasefulReader
 import Entry
+import Control.Parallel.Strategies
 
+data ResourceStat = ResourceStat {
+      count  :: Integer            -- Frequency of this resource in the data set
+    , params :: M.Map String Integer -- Key and its number of occurrences
+} deriving (Show,Eq)
+
+instance NFData ResourceStat where
+  rnf (ResourceStat x y) = rnf x `seq` rnf y
 
 main = do
   files <- getArgs
   result <- processEverything modifier folder combiner files
   putStrLn $ show result
   
-modifier :: Entry -> (String,Integer)
-modifier e = (exportURLWithoutParams $ url e,1)
-folder = fromListWith (+)
-combiner = unionsWith (+)
+-- |Builds "initial" frequency table one resource and its parameters.
+modifier :: Entry -> (String,ResourceStat)
+modifier e = (exportURLWithoutParams $ url e,ResourceStat 1 paramMap)
+    where paramNames = map fst $ url_params $ url e
+          paramMap = M.fromList $ zip paramNames [1..]
+
+folder = M.fromListWith reduceTwo
+combiner = M.unionsWith reduceTwo
+
+reduceTwo :: ResourceStat -> ResourceStat -> ResourceStat
+reduceTwo (ResourceStat c_1 p_1) (ResourceStat c_2 p_2) =
+    ResourceStat (c_1+c_2) (M.unionWith (+) p_1 p_2)
