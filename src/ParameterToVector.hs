de831b4ae7a0d2d96643259293834fc0d0232038
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-05-16 11:43:52 +0300

    Functions do compile, but there are major issues in what they do.
    Zipping with numbers do not work because the order of encountered
    resources do vary.  That can be fixed with resource map.

--- src/ParameterToVector.hs
+++ src/ParameterToVector.hs
@@ -1,45 +1,56 @@
 module ParameterToVector where
 
 import System.IO
+import Data.List
 import Data.List.Split
-import PhasefulReader
+import Data.Binary
+import Control.DeepSeq
 import qualified Data.Set as S
 import qualified Data.Map as M
 import qualified Entry as E
+import qualified Data.ByteString.Lazy.Char8 as B
+import PhasefulReader
+import Ngram
 
 processFile :: FilePath -> FilePath -> IO ()
 processFile siteInfoF prefix = do
   siteInfo <- decodeFile siteInfoF
   let res = resourceMap siteInfo
-  
+  writeResources (prefix++"resources.txt") res
+
+writeResources file res = writeFile file $ show $ M.toList res
+
 resourceMap :: M.Map String (M.Map String [Ngram Char])
             -> M.Map String Int
-resourceMap m = M.fromDistinctAscList $ zip (keys m) [1..]
-  
-processInfile :: FilePath -> FilePath -> (Entry -> (a,b)) -> IO ()
+resourceMap m = M.fromDistinctAscList $ zip (M.keys m) [1..]
+
+processInfile :: (Ord a, NFData a, NFData b, Binary b) =>
+                 FilePath -> FilePath -> (E.Entry -> (a,b)) -> IO ()
 processInfile file prefix f = do
   es <- readSerialFile file
   let chunks = map combineListToMap $ splitEvery entryChunkSize $ map f es
   mapM_ (writeChunk prefix) chunks
   
 -- |Writes the given chunk to a files. A chunk contains multiple resources.
-writeChunk :: FilePath -> Map a [b] -> IO ()
+writeChunk :: Binary b => FilePath -> M.Map a [b] -> IO ()
 writeChunk prefix m = mapM_ (writeSingle prefix) list
-    where list = zipWith [1..] (elems m)
+    where list = zip [1..] (M.elems m)
 
 -- |Writes one resource data to output file.
-writeSingle :: FilePath -> (Int, [a]) -> IO ()
+writeSingle :: Binary a => FilePath -> (Int, [a]) -> IO ()
 writeSingle prefix (i,as) = do
-  h <- openFile (prefix ++ show i)
+  h <- openFile (prefix ++ show i) AppendMode
   mapM_ ((B.hPut h) . encode) as
-  closeFile h
+  hClose h
 
 -- |Groups a list to a map where fst is the key.
-combineListToMap :: [(a,b)] -> Map a [b]
+combineListToMap :: (Ord a, NFData a, NFData b) => 
+                    [(a,b)] -> M.Map a [b]
 combineListToMap xs = foldl' updateListMap M.empty xs
 
 -- |Inserts element into list efficently (with no concat)
-updateListMap :: Map a [b] -> (a,b) -> Map a [b]
+updateListMap :: (Ord a, NFData a, NFData b) =>
+                 M.Map a [b] -> (a,b) -> M.Map a [b]
 updateListMap m (k,v) = m `deepseq` (k,v) `deepseq` M.insert k (v:vs) m
     where vs = M.findWithDefault [] k m
 
@@ -48,8 +59,8 @@ updateListMap m (k,v) = m `deepseq` (k,v) `deepseq` M.insert k (v:vs) m
 entryChunkSize = 1000
 
 -- |Filter which keeps all those "interesting" resources. Not tested.
-filterInteresting :: [String] -> Entry -> Bool
+filterInteresting :: [String] -> [E.Entry] -> [E.Entry]
 filterInteresting rs es = set `deepseq` myFilter
     where set = S.fromList rs
-          myFilter = filter (test . exportURLWithoutParams . E.url) es
+          myFilter = filter (test . E.exportURLWithoutParams . E.url) es
           test x = S.member x set
