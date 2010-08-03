2e707738609a150165328001b82abfb1fbd88a55
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-08-03 15:30:31 +0300

    Linepicker works standalone, too.

--- src/LinePicker.hs
+++ src/LinePicker.hs
@@ -1,5 +1,7 @@
-module LinePicker where
+module Main where
 
+import System.Environment (getArgs)
+import Control.Monad (when)
 import RandomHelpers (pick)
 import System.Random (newStdGen)
 import qualified Data.ByteString.Lazy.Char8 as B
@@ -12,3 +14,11 @@ pickLines n fromFile toFile = do
   file <- B.readFile fromFile
   let picked = pick n (B.lines file) gen
   B.writeFile toFile $ B.unlines picked
+
+main = do
+  args <- getArgs
+  when (length args /= 3) $ error "Usage: linepicker maximum input_file output_file"
+  let n = read (args !! 0) :: Int
+  
+  pickLines n (args !! 1) (args !! 2)
+
