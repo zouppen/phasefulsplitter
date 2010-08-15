39ad0f716284f44a22d15ddb6f827bf5262c22ce
Joel Lehtonen <joel.lehtonen@iki.fi>
2010-08-15 18:45:20 +0300

    Improved backtracking functions for everyday usage.

--- src/LineInfoConvert.hs
+++ src/LineInfoConvert.hs
@@ -31,15 +31,31 @@ getLogLine :: FileMap -> LineInfo -> IO B.ByteString
 getLogLine fMap lineInfo = do
   contents <- B.readFile fileName
   return $ genericIndex (B.lines $ decompress contents) line
-    where fileInfo = lineInfo{lineNo = 0}
-          fileName = (M.!) fMap fileInfo
+    where fileName = getFileName fMap lineInfo
           line = lineNo lineInfo - 1  -- LineInfo indices do start from one.
           
-printLogLines :: FilePath -> [LineInfo] -> IO ()
-printLogLines fileList xs = do
+-- |Returns file name where that lineinfo is found.
+getFileName :: FileMap -> LineInfo -> FilePath
+getFileName fMap lineInfo = (M.!) fMap fileInfo
+  where fileInfo = lineInfo{lineNo = 0}
+
+
+getLogLines :: FilePath -> [LineInfo] -> IO [B.ByteString]
+getLogLines fileList xs = do
   fMap <- readFileMap fileList
   logLines <- mapM (getLogLine fMap) xs
-  B.putStr $ B.unlines $ zipWith niceLineOutput xs logLines
+  return $ zipWith niceLineOutput xs logLines
+
+printLogLines :: FilePath -> [LineInfo] -> IO ()
+printLogLines fileList xs = do
+  ls <- getLogLines fileList xs
+  B.putStr $ B.unlines ls
+
+-- |Writes anomalies to a given file.
+saveLogLines :: FilePath -> FilePath -> [LineInfo] -> IO ()
+saveLogLines fileList outFile xs = do
+  ls <- getLogLines fileList xs
+  B.writeFile outFile $ B.unlines ls
 
 -- |Formats LineInfo and a log file line into a nice looking ByteString.
 niceLineOutput :: LineInfo -> B.ByteString -> B.ByteString
@@ -48,13 +64,13 @@ niceLineOutput (LineInfo a b c) line =
     where toBS x = B.pack $ show x
 
 -- |"Zipola format" is Matlab text output I often get from my workmate.
-readZipolaFormat :: String -> (Integer,LineInfo)
-readZipolaFormat line = (readI 3,LineInfo (readI 0) (readI 1) (readI 2))
-  where elems = split (dropInitBlank . condense . dropDelims $ oneOf " \t") line
+readZipolaFormat :: String -> LineInfo
+readZipolaFormat line = LineInfo (readI 0) (readI 1) (readI 2)
+  where elems = split (dropInitBlank . condense . dropDelims $ oneOf ", \t") line
         readI i = read $ elems !! i
 
 -- |Read an entire "Zipola formatted" file.
-readZipolaFile :: FilePath -> IO [(Integer,LineInfo)]
+readZipolaFile :: FilePath -> IO [LineInfo]
 readZipolaFile f = do
   contents <- readFile f
   return $ map readZipolaFormat $ filter (not.empty) $ lines contents
@@ -62,3 +78,18 @@ readZipolaFile f = do
 empty [] = True
 empty ('#':_) = True
 empty _ = False
+
+-- |Backtracks a list of files. Takes in file list and infile-outfile pairs.
+backtrackCSV :: FilePath -> [(FilePath,FilePath)] -> IO () 
+backtrackCSV fileList target = do
+  lineInfos <- mapM readZipolaFile inFiles
+  mapM_ saveLogLines' $ zip outFiles lineInfos
+  where inFiles = map fst target
+        outFiles = map snd target
+        saveLogLines' = uncurry (saveLogLines fileList)
+        
+backtrackCSVnumeric :: (Integral a) => FilePath -> FilePath -> FilePath -> [a] -> IO ()
+backtrackCSVnumeric fileList inPrefix outPrefix resources = do
+  backtrackCSV fileList $ zip inFiles outFiles
+  where inFiles = map ((inPrefix++).show) resources
+        outFiles = map ((outPrefix++).show) resources
